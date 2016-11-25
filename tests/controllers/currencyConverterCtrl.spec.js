af3ad8a660405405edcc3c4bf6233f702f768f9e
andela-ymakanjuola <yinka.makanjuola@gmail.com>
2016-11-25 16:56:15 +0100

    update testsr

--- tests/controllers/currencyConverterCtrl.spec.js
+++ tests/controllers/currencyConverterCtrl.spec.js
@@ -1,12 +1,18 @@
 describe('currencyConverterCtrl', function() {
+  var $controller, scope;
+  beforeEach(module('app'));
 
-  beforeEach(module('app'))
+  beforeEach(inject(function(_$controller_, _$rootScope_) {
+    $controller = _$controller_;
+    scope = _$rootScope_.$new();
+    
+  }))
 
   it('should expect the currency converter controller to be defined', function() {
 
   });
 
   it('should get a list of currencies', function() {
-    
+    expect(scope.currencies).toBeDefine()
   })
 })
--- tests/services/currencyConverterService.spec.js
+++ tests/services/currencyConverterService.spec.js
@@ -2,13 +2,13 @@ describe('currencyConverterService', function() {
   beforeEach(module('app'));
   it('should expect the service to be defined', inject(function(currencyConverterService) {
     expect(currencyConverterService).toBeDefined();
-  });
-
-  it('should get the rates from https://api.fixer.io/latest', inject(function(currencyConverterService) {
-
   }));
 
-  it('should get the rates from https://api.fixer.io/latest', inject(function(currencyConverterService) {
+  describe('getUSDRates', function() {
+    it('should get the rates from https://api.fixer.io/latest', inject(function(currencyConverterService) {
+      ratesData = {}
+      expect(currencyConverterService.getRates()).toBe(ratesData)
+    }));
+  })
 
-  }));
 })
