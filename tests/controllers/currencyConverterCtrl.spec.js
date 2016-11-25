da16b7235c3e50e088622d5dce76ac9e27475da4
andela-ymakanjuola <yinka.makanjuola@gmail.com>
2016-11-25 16:17:42 +0100

    Add test files and home page
    include tests for controllers and services
    build a skeletal frame of the home page

---
+++ tests/controllers/currencyConverterCtrl.spec.js
@@ -0,0 +1,12 @@
+describe('currencyConverterCtrl', function() {
+
+  beforeEach(module('app'))
+
+  it('should expect the currency converter controller to be defined', function() {
+
+  });
+
+  it('should get a list of currencies', function() {
+    
+  })
+})
---
+++ tests/services/currencyConverterService.spec.js
@@ -0,0 +1,14 @@
+describe('currencyConverterService', function() {
+  beforeEach(module('app'));
+  it('should expect the service to be defined', inject(function(currencyConverterService) {
+    expect(currencyConverterService).toBeDefined();
+  });
+
+  it('should get the rates from https://api.fixer.io/latest', inject(function(currencyConverterService) {
+
+  }));
+
+  it('should get the rates from https://api.fixer.io/latest', inject(function(currencyConverterService) {
+
+  }));
+})
