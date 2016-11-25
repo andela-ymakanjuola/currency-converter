afabbe6307066fb99f8a90619cc6acaae72375ea
andela-ymakanjuola <yinka.makanjuola@gmail.com>
2016-11-25 17:39:10 +0100

    display data and conversions

--- app/controllers/currencyConverterCtrl.js
+++ app/controllers/currencyConverterCtrl.js
@@ -1,6 +1,26 @@
 angular.module('app').controller('currencyConverterCtrl', function($scope, currencyConverterService) {
   $scope.currencies = [];
+  $scope.currencyAmount = 0;
+  $scope.usdCurency = 0
+  var rates = {};
+  
   currencyConverterService.getUSDRates().then(function(data){
     $scope.currencies = Object.keys(data.data['rates']);
-  })
+  }).catch(function(error) {
+    if(error) {
+      console.log("error getting currencies", error);
+    }
+  });
+
+  $scope.convertCurrencyToUsd = function(currency) {
+     currencyConverterService.getUSDRates().then(function(data) {
+      console.log(currency)
+      rates = data.data['rates'];
+      $scope.usdCurency = $scope.currencyAmount/$scope.rates[currency];
+    }).catch(function(error) {
+      if(error) {
+        console.log("error getting rates", error)
+      }
+    });
+  }
 })
